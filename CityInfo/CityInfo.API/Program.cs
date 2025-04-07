a75ea864c2abb07a8d768bbec397d5f47cefdcdd
AgisMoralis <amoralis94@gmail.com>
2025-04-07 16:33:10 +0200

    - Remove the default 'WeatherForecast' controller - Use attribute-based routing and create a new 'City' controller

--- CityInfo/CityInfo.API/Program.cs
+++ CityInfo/CityInfo.API/Program.cs
@@ -3,19 +3,17 @@ using Microsoft.AspNetCore.Authentication.Certificate;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddAuthentication(
+       CertificateAuthenticationDefaults.AuthenticationScheme)
+       .AddCertificate();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication(
-       CertificateAuthenticationDefaults.AuthenticationScheme)
-       .AddCertificate();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline (middleware pipeline).
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -26,8 +24,15 @@ app.UseAuthentication();
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints => {
+    endpoints.MapControllers();
+});
+
+// Latest .NET uses the top-level route registrations instead of "UseEndpoints"
+//app.MapControllers();
 
 app.Run();
