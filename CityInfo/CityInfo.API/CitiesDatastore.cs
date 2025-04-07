cd9f0cc78a11aa5d6dbbb09fc66122fa990c0848
AgisMoralis <amoralis94@gmail.com>
2025-04-07 17:12:50 +0200

    - Create 'City' model (DTO class) used as in-memory - Create "Cities" datastore with dummy data

---
+++ CityInfo/CityInfo.API/CitiesDatastore.cs
@@ -0,0 +1,37 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDatastore
+    {
+        public List<CityDto> Cities { get; set; }
+
+        public static CitiesDatastore Current { get; set; } = new CitiesDatastore();
+
+        public CitiesDatastore()
+        {
+            // Dummy data
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "NYC",
+                    Description = "City with a lot of people"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "City with a big port"
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Antwerp",
+                    Description = "City with Eiffel Tower"
+                }
+            };
+        }
+    }
+}
--- CityInfo/CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo/CityInfo.API/Controllers/CitiesController.cs
@@ -3,16 +3,13 @@
 namespace CityInfo.API.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class CitiesController : ControllerBase
     {
-        [HttpGet("api/cities")]
-        public JsonResult GetCities() 
+        [HttpGet()]
+        public JsonResult GetCities()
         {
-            return new JsonResult(
-                new List<object> { 
-                    new { id = 1, Name = "New York City"},
-                    new { id = 2, Name = "Brussels"}
-                });
+            return new JsonResult(CitiesDatastore.Current.Cities);
         }
     }
 }
