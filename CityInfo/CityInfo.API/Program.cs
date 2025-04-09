87e361d4a6d2eb1389eaf6b01120bebe4c063b8e
AgisMoralis <amoralis94@gmail.com>
2025-04-09 19:41:25 +0200

    - Example of defining inside the server error responses additional fields and detailed info messages - The API returns a specific status code when a consumer asks for a specific type of content representation that is not supported support - Add Xml Data Contract Formatters for the content representation - Create a new "Files" controller that is populated with 2 images - Add FileExtensionContentTypeProvider singleton service to help us extract the content type of any file to be used on any returned FileContentResult

--- CityInfo/CityInfo.API/Program.cs
+++ CityInfo/CityInfo.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.AspNetCore.StaticFiles;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,10 +7,28 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthentication(
        CertificateAuthenticationDefaults.AuthenticationScheme)
        .AddCertificate();
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    // When a consumer asks for a specific type of content representation that the API does not support, the API should return a status code about that
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters();
+
+// Example of defining inside the server error responses additional fields and detailed info messages
+//builder.Services.AddProblemDetails(options =>
+//{
+//    options.CustomizeProblemDetails = ctx => 
+//    {
+//        ctx.ProblemDetails.Extensions.Add("additionalInfo", "Additional info added for the example");
+//        ctx.ProblemDetails.Extensions.Add("server", Environment.MachineName);
+//    };
+//});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Use the FileExtensionContentTypeProvider servive to get the extension type of any file, to use for the content type of any returned FileContentResult
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
