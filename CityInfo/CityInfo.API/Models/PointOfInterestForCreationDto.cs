0cbf423668faa3ddff0dd9aca8e3913072c01ed8
AgisMoralis <amoralis94@gmail.com>
2025-04-12 20:27:50 +0200

    Adding custom messages in the error body, returned with the "bad request" response of the application when the validation of the data failed

--- CityInfo/CityInfo.API/Models/PointOfInterestForCreationDto.cs
+++ CityInfo/CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -4,11 +4,11 @@ namespace CityInfo.API.Models
 {
     public class PointOfInterestForCreationDto
     {
-        [Required]
-        [MaxLength(10)]
+        [Required(ErrorMessage = "You should provide a name value for the new Point of Interest.")]
+        [MaxLength(10, ErrorMessage = "The name value of the new Point of Interest shall not exceed the maximum length of 10 characters.")]
         public string Name { get; set; } = string.Empty;
 
-        [MaxLength(10)]
+        [MaxLength(10, ErrorMessage = "The description value of the new Point of Interest shall not exceed the maximum length of 10 characters.")]
         public string? Description { get; set; } = null;
     }
 }
