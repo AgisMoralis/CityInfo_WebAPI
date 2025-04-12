e5f0ecbc33619a6472209786eea815fc0af96f59
AgisMoralis <amoralis94@gmail.com>
2025-04-12 20:22:33 +0200

    - Create the "PointOfInterestForCreation" DTO class to be used for a POST HTTP request, to create new resources - Create the new "CreatePointOfInterest()" action to be invoked when there is a POST request, that uses the new DTO class - Create some basic validation rules on the two properties of the "PointOfInterestForCreation" DTO class, based on validation attributes

--- CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,6 +1,5 @@
 ﻿using CityInfo.API.Models;
 using Microsoft.AspNetCore.Mvc;
-
 namespace CityInfo.API.Controllers
 {
     [ApiController]
@@ -14,7 +13,7 @@ namespace CityInfo.API.Controllers
             return city is null ? NotFound() : Ok(city.PointsOfInterest);
         }
 
-        [HttpGet("{pointofinterestid}")]
+        [HttpGet("{pointofinterestid}", Name = "GetPointOfInterest")]
         public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
         {
             var city = CitiesDatastore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -25,6 +24,42 @@ namespace CityInfo.API.Controllers
 
             var pointOfInterestToReturn = city.PointsOfInterest.FirstOrDefault(c => c.Id == pointOfInterestId);
             return pointOfInterestToReturn is null ? NotFound() : Ok(pointOfInterestToReturn);
+        }
+
+        [HttpPost]
+        public ActionResult<PointOfInterestDto> CreatePointOfInterest(int cityId, PointOfInterestForCreationDto pointOfInterest)
+        {
+            // Because of the [ApiController] attribute, the annotations are automatically checked during model binding
+            // that affects the ModelState dictionary. The [ApiController] attribute ensures, in case of an invalid ModelState,
+            // that a bad request is returned as a response, with the validation error details in the body of the response
+            // if (!ModelState.IsValid) 
+            // {
+            //     return BadRequest();
+            // }
+
+            var city = CitiesDatastore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city is null)
+            {
+                return NotFound();
+            }
+
+            // Create a new Point of Interest and add it in the resources
+            var maxPointOfInterestId = city.PointsOfInterest.Max(c => c.Id);
+            var newPointOfInterest = new PointOfInterestDto()
+            {
+                Id = maxPointOfInterestId + 1,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description,
+            };
+            city.PointsOfInterest.Add(newPointOfInterest);
+
+            return CreatedAtRoute("GetPointOfInterest",
+                new
+                {
+                    cityId = cityId,
+                    pointOfInterestId = newPointOfInterest.Id
+                },
+                newPointOfInterest);
         }
     }
 }
---
+++ CityInfo/CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        [Required]
+        [MaxLength(10)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(10)]
+        public string? Description { get; set; } = null;
+    }
+}
